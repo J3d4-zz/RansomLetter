b2490b17cb1592b82f4beb1f89c73261c06dfd05
J3d4 <bakos.mrtn@gmail.com>
2020-12-18 14:37:02 +0100

    asdasd
    asdf

--- ransomTest.js
+++ ransomTest.js
@@ -5,12 +5,12 @@ async function TestSuite(){
     con.write('Adjon meg egy szot:')
     var input = await con.read();
     con.write('Adjon meg egy mondatot:')
-    var note = await con.read();
+    var text = await con.read();
 
     var cleanTest = ransomLetter.Clean(input);
-    con.write("A letisztitott szo array-re bontva: " + cleanTest + ".")
+    con.write("A letisztitott szo tombre bontva: " + cleanTest + ".")
 
-    var ransomTest = ransomLetter.CanBuild(input, note);
+    var ransomTest = ransomLetter.CanBuild(input, text);
     con.write("A megadott szo kirakhato-e a megadott mondatbol: " + ransomTest + ".")
 }
 
